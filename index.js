1e91bacb41bbe60e5501594c869824daca8685d7
Arthur-codeX <daniel.karanja239@gmail.com>
2022-03-16 02:35:41 +0300

    commit tree 15ba5cf61a529ab0f742291b5be62eb818e29c5c

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 function batteries() {
+  let totalBatteries = 0;
   return batteryBatches.reduce((totalBatteries, batteryBatches) => {
     return totalBatteries + batteryBatches;
-  }, 0);
+  });
 }
 
 console.log(batteries());
